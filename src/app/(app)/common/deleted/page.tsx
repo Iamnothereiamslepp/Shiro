21c1d589456dd9e0305223b15b7a51981a913b7b
Iamnothereiamslepp <128118699+Iamnothereiamslepp@users.noreply.github.com>
2025-04-02 23:22:20 +0800

    Update page.tsx

--- src/app/(app)/common/deleted/page.tsx
+++ src/app/(app)/common/deleted/page.tsx
@@ -7,10 +7,11 @@ export default async function PageDeleted() {
   return (
     <NormalContainer>
       <div className="mt-[250px] flex h-full flex-col items-center justify-center">
-        <h1 className="text-4xl font-bold">此页面已被删除</h1>
+        <h1 className="text-4xl font-bold">Σ（ﾟдﾟlll）此頁面已被刪除</h1>
+        <h2 className="text-4xl font-bold">可能作者不想讓你知道o(╯□╰)o</h2>
 
         <StyledButton className="mt-8">
-          <Link href="/">返回首页</Link>
+          <Link href="/">返回首頁↻</Link>
         </StyledButton>
       </div>
     </NormalContainer>
